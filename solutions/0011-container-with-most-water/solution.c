73ff6ada7f095351634784f2ca06ffb4c048d919
Krish <krishgupta3879@gmail.com>
2026-05-15 23:00:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.8 MB (49.41%)

---
+++ solutions/0011-container-with-most-water/solution.c
@@ -0,0 +1,20 @@
+int maxArea(int* height, int heightSize) {
+    int max_area = 0;
+    int left = 0;
+    int right = heightSize - 1;
+    while (left<right){
+        int width=right-left;
+        int h=height[left]<height[right] ? height[left] : height[right];
+
+        int current_area=width*h;
+        if(current_area>max_area){
+            max_area=current_area;
+        }
+        if(height[left]<height[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    return max_area;
+}
