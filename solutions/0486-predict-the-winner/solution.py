35087bd89143710d90dd56d64b0e86c95d946f78
Krish <krishgupta3879@gmail.com>
2026-08-02 07:09:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (78.41%)

--- solutions/0486-predict-the-winner/solution.py
+++ solutions/0486-predict-the-winner/solution.py
@@ -1,18 +1,28 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        memo = {}
+        n = len(nums)
+        dp = list(nums)  # Base case for length 1
+        
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i] = max(nums[i] - dp[i + 1], nums[j] - dp[i])
+                
+        return dp[0] >= 0
+        
+        # memo = {}
 
-        def helper(i, j):
-            if i == j:
-                return nums[i]
-            if (i, j) in memo:
-                return memo[(i, j)]
+        # def helper(i, j):
+        #     if i == j:
+        #         return nums[i]
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
 
-            pick_left = nums[i] - helper(i + 1, j)
-            pick_right = nums[j] - helper(i, j - 1)
+        #     pick_left = nums[i] - helper(i + 1, j)
+        #     pick_right = nums[j] - helper(i, j - 1)
 
-            memo[(i, j)] = max(pick_left, pick_right)
-            return memo[(i, j)]
+        #     memo[(i, j)] = max(pick_left, pick_right)
+        #     return memo[(i, j)]
 
-        return helper(0, len(nums) - 1) >= 0
+        # return helper(0, len(nums) - 1) >= 0
 
