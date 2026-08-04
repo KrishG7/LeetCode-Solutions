002c4adc9bdba3f2c29154028c5459c03c38f888
Krish <krishgupta3879@gmail.com>
2026-08-04 10:29:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (18.93%)

---
+++ solutions/4107-find-missing-elements/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        res = []
+        nums.sort()
+        j = 0
+        for i in range(nums[0], nums[-1]):
+            if nums[j] != i:
+                res.append(i)
+            else:
+                j += 1
+        return res
+
